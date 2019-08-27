33637aa6144a0ea19ab65a9485af6eac51922796
AliRezaali <AliRezaali.Web.78@gmail.com>
2019-08-27 21:55:02 +0430

    Fix Login

--- js/login.js
+++ js/login.js
@@ -1,59 +1,56 @@
-$(function(){
-    $('#loginbtn').on('click',function(event){
+$(function () {
+    $('#loginbtn').on('click', function (event) {
         event.preventDefault();
 
         //validation
-        let user=$('#user').val();
-        let pass=$('#pass').val();
-        let isValid=  validateForm(user,pass);
+        let user = $('#user').val();
+        let pass = $('#pass').val();
+        let isValid = validateForm(user, pass);
 
-        if(isValid){
-            login(user,pass);
+        if (isValid) {
+            login(user, pass);
         }
-      
+
     });
 });
 
-function validateForm(user,pass){
-    if(user.trim()==='' || pass.trim()==='')
-    {
+function validateForm(user, pass) {
+    if (user.trim() === '' || pass.trim() === '') {
         $('#empty-input').show();
         return false;
     }
-    else{
+    else {
         $('#empty-input').hide();
         return true;
     }
 
 }
 
-function login(username,password){
-    
-    if(!cookies.test()){
+function login(username, password) {
+
+    if (!cookies.test()) {
         $('#cookie-support').show();
         return;
     }
-    else
-    {
+    else {
         $('#cookie-support').hide();
     }
 
-    if(username !=='admin' && password!=='admin')
-    {
+    if (username == 'admin' && password == 'admin') {
+        $('#wrong-userpass').hide();
+    }
+    else {
         $('#wrong-userpass').show();
         return;
     }
-    else
-        $('#wrong-userpass').hide();
-
 
     //success
-    let user = new User(username,'احمد','رضایی','ادمین','002126256','asdasdasdasdasd','asdasdasdasds');
-    cookies.set('user',user);
-    
-    cookies.set('login',true);
-//    let baseUrl = getBaseUrl();
-//    baseUrl+="proj/"; 
+    let user = new User(username, 'احمد', 'رضایی', 'ادمین', '002126256', 'asdasdasdasdasd', 'asdasdasdasds');
+    cookies.set('user', user);
+
+    cookies.set('login', true);
+    //    let baseUrl = getBaseUrl();
+    //    baseUrl+="proj/"; 
     window.location.replace("main.html#/home");
 
     //fail
@@ -64,15 +61,15 @@ function getBaseUrl() {
     var re = new RegExp(/^.*\//);
     return re.exec(window.location.href);
 }
-class User{
-    constructor(username,name,fname,role,code,token,pkey){
-        this.username=username;
-        this.name=name;
-        this.role=role;
-        this.code= code;
-        this.fname=fname;
-        this.token=token;
-        this.pkey=pkey;
+class User {
+    constructor(username, name, fname, role, code, token, pkey) {
+        this.username = username;
+        this.name = name;
+        this.role = role;
+        this.code = code;
+        this.fname = fname;
+        this.token = token;
+        this.pkey = pkey;
     }
-    
+
 }
